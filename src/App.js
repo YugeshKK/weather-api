427b40e9519016de79b1202b8472c3efda826717
YKK <yugeshkk2001@gmail.com>
2022-09-01 11:49:43 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ function App() {
   const [data, setData] = useState({})
   const [location, setLocation] = useState('')
 
-  const url=`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=1ab4a8a8b1f205e91e531ee8298ccb2a`
-
+  const url=`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=1ab4a8a8b1f205e91e531ee8298ccb2a&units=metric`
   const searchLocation = (event) => {
     if (event.key === 'Enter') {
       axios.get(url).then((response) => {
@@ -36,7 +35,7 @@ function App() {
             <p>{data.name}</p>
           </div>
           <div className="temp">
-            {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
+            {data.main ? <h1>{data.main.temp.toFixed()}°C</h1> : null}
           </div>
           <div className="description">
             {data.weather ? <p>{data.weather[0].main}</p> : null}
@@ -46,7 +45,7 @@ function App() {
         {data.name !== undefined &&
           <div className="bottom">
             <div className="feels">
-              {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°F</p> : null}
+              {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°C</p> : null}
               <p>Feels Like</p>
             </div>
             <div className="humidity">
